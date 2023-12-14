8b6c3df554f87a1487bf0b045acddfd67a6e5827
Anirudha B Shetty <ab007shetty@gmail.com>
2023-12-14 17:38:12 +0530

    Initial Commit

---
+++ settings.js
@@ -0,0 +1,465 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 500,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": true,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "7f5w8",
+ "map_data": {
+  "st1": {
+   "name": "Andaman and Nicobar Islands",
+   "shortname": "AN",
+   "comment": "",
+   "link": "",
+   "id": 1,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st2": {
+   "name": "Andhra Pradesh",
+   "shortname": "AP",
+   "comment": "",
+   "link": "",
+   "id": 2,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st3": {
+   "name": "Arunachal Pradesh",
+   "shortname": "AR",
+   "comment": "",
+   "link": "",
+   "id": 3,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st4": {
+   "name": "Assam",
+   "shortname": "AS",
+   "comment": "",
+   "link": "",
+   "id": 4,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st5": {
+   "name": "Bihar",
+   "shortname": "BR",
+   "comment": "",
+   "link": "",
+   "id": 5,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st6": {
+   "name": "Chandigarh",
+   "shortname": "CH",
+   "comment": "",
+   "link": "",
+   "id": 6,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st7": {
+   "name": "Chhattisgarh",
+   "shortname": "CT",
+   "comment": "",
+   "link": "",
+   "id": 7,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st8": {
+   "name": "Dadra and Nagar Haveli and Daman and Diu",
+   "shortname": "DD",
+   "comment": "",
+   "link": "",
+   "id": 8,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st9": {
+   "name": "National Capital Territory of Delhi",
+   "shortname": "DL",
+   "comment": "",
+   "link": "",
+   "id": 9,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st10": {
+   "name": "Goa",
+   "shortname": "GA",
+   "comment": "",
+   "link": "",
+   "id": 10,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st11": {
+   "name": "Gujarat",
+   "shortname": "GJ",
+   "comment": "",
+   "link": "",
+   "id": 11,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st12": {
+   "name": "Himachal Pradesh",
+   "shortname": "HP",
+   "comment": "",
+   "link": "",
+   "id": 12,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st13": {
+   "name": "Haryana",
+   "shortname": "HR",
+   "comment": "",
+   "link": "",
+   "id": 13,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st14": {
+   "name": "Jharkhand",
+   "shortname": "JH",
+   "comment": "",
+   "link": "",
+   "id": 14,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st15": {
+   "name": "Jammu and Kashmir",
+   "shortname": "JK",
+   "comment": "",
+   "link": "",
+   "id": 15,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st16": {
+   "name": "Karnataka",
+   "shortname": "KA",
+   "comment": "",
+   "link": "",
+   "id": 16,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st17": {
+   "name": "Kerala",
+   "shortname": "KL",
+   "comment": "",
+   "link": "",
+   "id": 17,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st18": {
+   "name": "Ladakh",
+   "shortname": "LA",
+   "comment": "",
+   "link": "",
+   "id": 18,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st19": {
+   "name": "Lakshadweep",
+   "shortname": "LD",
+   "comment": "",
+   "link": "",
+   "id": 19,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st20": {
+   "name": "Maharashtra",
+   "shortname": "MH",
+   "comment": "",
+   "link": "",
+   "id": 20,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st21": {
+   "name": "Meghalaya",
+   "shortname": "ML",
+   "comment": "",
+   "link": "",
+   "id": 21,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st22": {
+   "name": "Manipur",
+   "shortname": "MN",
+   "comment": "",
+   "link": "",
+   "id": 22,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st23": {
+   "name": "Madhya Pradesh",
+   "shortname": "MP",
+   "comment": "",
+   "link": "",
+   "id": 23,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st24": {
+   "name": "Mizoram",
+   "shortname": "MZ",
+   "comment": "",
+   "link": "",
+   "id": 24,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st25": {
+   "name": "Nagaland",
+   "shortname": "NL",
+   "comment": "",
+   "link": "",
+   "id": 25,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st26": {
+   "name": "Orissa",
+   "shortname": "OR",
+   "comment": "",
+   "link": "",
+   "id": 26,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st27": {
+   "name": "Punjab",
+   "shortname": "PB",
+   "comment": "",
+   "link": "",
+   "id": 27,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st28": {
+   "name": "Puducherry",
+   "shortname": "PY",
+   "comment": "",
+   "link": "",
+   "id": 28,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st29": {
+   "name": "Rajasthan",
+   "shortname": "RJ",
+   "comment": "",
+   "link": "",
+   "id": 29,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st30": {
+   "name": "Sikkim",
+   "shortname": "SK",
+   "comment": "",
+   "link": "",
+   "id": 30,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st31": {
+   "name": "Telangana",
+   "shortname": "TG",
+   "comment": "",
+   "link": "",
+   "id": 31,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st32": {
+   "name": "Tamil Nadu",
+   "shortname": "TN",
+   "comment": "",
+   "link": "",
+   "id": 32,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st33": {
+   "name": "Tripura",
+   "shortname": "TR",
+   "comment": "",
+   "link": "",
+   "id": 33,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st34": {
+   "name": "Uttar Pradesh",
+   "shortname": "UP",
+   "comment": "",
+   "link": "",
+   "id": 34,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st35": {
+   "name": "Uttarakhand",
+   "shortname": "UT",
+   "comment": "",
+   "link": "",
+   "id": 35,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st36": {
+   "name": "West Bengal",
+   "shortname": "WB",
+   "comment": "",
+   "link": "",
+   "id": 36,
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  }
+ },
+ "points": {
+  "p0": {
+   "x": 111.75,
+   "y": 34.5,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "1",
+   "shortname": "Siachen",
+   "pointType": "",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "1st women officer deployed"
+  },
+  "p1": {
+   "x": 146.25,
+   "y": 91.5,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Sunni Dam",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "Cabinet committee on Economic Affairs approved investment for Sunni Dam hydroelectric project on SATLUJ River"
+  },
+  "p2": {
+   "x": 272.589,
+   "y": 187.415,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Nalanda",
+   "pointType": "star",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "ASI discovered two 1200 yrs old miniature votive Stupa at Nalanda"
+  },
+  "p3": {
+   "x": 302.25,
+   "y": 237,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Bharatpur",
+   "pointType": "star",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "1.Excavations revealed Buddhist monastry complex"
+  },
+  "p4": {
+   "x": 384.5,
+   "y": 416.009,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Great Nicobar Island",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "Centre prepares ₹41,000 crore International Container Transhipment port"
+  },
+  "p5": {
+   "x": 133.5,
+   "y": 432.75,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Kereala",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "1.White tufted royal butterfly spotted at Kalliyad\r\n2. Kollam district has become India's 1st Constitution literate district\r\n3. Wayanad becomes 1st district in the country to provide basic documents to all tribal people"
+  },
+  "p6": {
+   "x": 94.5,
+   "y": 296.25,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Maharashtra",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "New Plateau type discovered"
+  },
+  "p7": {
+   "x": 43.5,
+   "y": 222,
+   "color": "#000000",
+   "colorOver": "#ffffff",
+   "name": "",
+   "shortname": "Juna Khatiya(Kutch District)",
+   "pointType": "star",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": "Excavations found rows of graves from 3200 BCE to 2600 BCE"
+  }
+ }
+};
